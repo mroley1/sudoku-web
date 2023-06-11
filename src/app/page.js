0650f7d0b52d0388ee4e6a65e693650917862358
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-06-10 23:24:59 -0400

    layout draft

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,44 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+const NUMPADTOPROW = [
+  {name: "1", value: 1},
+  {name: "2", value: 2},
+  {name: "3", value: 3},
+  {name: "4", value: 4},
+  {name: "5", value: 5},
+]
+
+const NUMPADBOTTOMROW = [
+  {name: "6", value: 6},
+  {name: "7", value: 7},
+  {name: "8", value: 8},
+  {name: "9", value: 9},
+  {name: "X", value: 0},
+]
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className={styles.main_flex}>
+        <div className={styles.board_container}>
+          <div className={styles.board}></div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.input_container}>
+          <div className={styles.num_pad}>
+            {NUMPADTOPROW.map(num => <NumPadButton num={num} />)}
+            {NUMPADBOTTOMROW.map(num => <NumPadButton num={num} />)}
+          </div>
+        </div>
       </div>
     </main>
   )
+}
+
+export function NumPadButton({num: num}) {
+  return (
+    <div key={num.name} className={styles.num_pad_key}>
+      <span>{num.name}</span>
+    </div>
+    )
 }
